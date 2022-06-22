39a880995ff706be204791a576075c249c772062
sqrt_-1 <91781190+sqrt2802@users.noreply.github.com>
2022-06-22 15:44:23 +0800

    init

---
+++ config.rb
@@ -0,0 +1,9 @@
+http_path = "/"
+css_dir = "stylesheets"
+sass_dir = "stylesheets"
+images_dir = "images"
+javascripts_dir = "javascripts"
+output_style = :expanded
+environment = :production
+line_comments = false
+color_output = false
